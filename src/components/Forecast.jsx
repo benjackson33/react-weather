6cef0692a8a2b04b74c57d516057cf0095906b3e
Ben Jackson <benjacksonsmac@gmail.com>
2023-08-09 19:31:40 +1200

    server refactor

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,6 +1,54 @@
-export default function Forecast() {
-    return (
-        <>
-        </>
+import { useEffect, useState } from "react";
+import "./Forecast.css"
+
+export default function Forecast({lat, lon, API}) {
+  const [weatherData, setWeatherData] = useState([]);
+  //   const [temp, setTemp] = useState([]);
+  //   const [description, setDescription] = useState([]);
+
+  // console.log(lat + lon);
+  console.log(API);
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API}`
     )
+      .then((res) => res.json())
+      .then((data) => {
+        setWeatherData(data.list);
+
+        console.log(data.list);
+      })
+      .catch((error) => {
+        console.error("Error fetching forecast data:", error);
+      });
+  }, []);
+
+  const groupedByDate = weatherData.reduce((acc, item) => {
+    const date = item.dt_txt.split(" ")[0]; // Extract date from dt_txt
+    if (!acc[date]) {
+      acc[date] = [];
+    }
+    acc[date].push(item);
+    return acc;
+  }, {});
+
+  return (
+    <div>
+      
+      {Object.keys(groupedByDate).map((date) => (
+        <div className="weather-card" key={date}>
+          <h2>{date}</h2>
+          {groupedByDate[date].map((item, index) => (
+            <div  key={index}>
+              <p>{item.dt_txt.split(" ")[1]}</p>
+              <p>{Math.trunc(item.main.temp - 273.15)}&deg;C</p>   
+              <p>{item.weather[0].description}</p>
+              {/* Add more data points you want to display */}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,45 +1,77 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 import "./Search.css";
+import axios from "axios";
 import CurrentWeather from "./CurrentWeather";
 import Forecast from "./Forecast";
 
-const API_KEY = "";
-const GEO_API_URL = "http://api.openweathermap.org/geo/1.0/direct";
-const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather";
+// const API_KEY = "c3606b4cb3e49273488081b7e1a65c2a";
+// const GEO_API_URL = "http://api.openweathermap.org/geo/1.0/direct";
+// const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export default function Search() {
   const [location, setLocation] = useState("London");
-
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
   const [weather, setWeather] = useState("");
   const [description, setDescription] = useState("");
   const [temp, setTemp] = useState(0);
+  const [showWeather, setShowWeather] = useState(true);
+  const [showForecast, setShowForecast] = useState(false);
+  // const [showForecast, setShowForecast] = useState(false);
 
-  useEffect(() => {
-    handleSearch();
-  }, []);
+  // useEffect(() => {
+  //   handleSearch();
+  // }, []);
 
-  const handleSearch = () => {
-    fetch(`${GEO_API_URL}?q=${location}&limit=5&appid=${API_KEY}`)
-      .then((res) => res.json())
-      .then(([data]) => {
+  
+
+  const handleSearch = async () => {
+  //  const url = "/location"
+  //  const res = await fetch(url)
+  //  const data  = await res.json()
+  //  console.log(data);
+
+    axios.get(`http://localhost:3001/${location}`)
+      .then((res) => {
+        // console.log(res);
+        const [data] = res.data;
         if (data) {
           const { lat, lon } = data;
+          // console.log(data);
+          // console.log(lat);
+          setLatitude(lat);
+          setLongitude(lon);
           fetchWeatherData(lat, lon);
         }
+      })
+      .catch((error) => {
+        console.error("Error fetching location data:", error);
       });
   };
 
   const fetchWeatherData = (lat, lon) => {
-    fetch(`${WEATHER_API_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-      .then((res) => res.json())
-      .then((data) => {
+    axios.get(`http://localhost:3001/location/weather/${lat}/${lon}`)
+      .then((res) => {
+        console.log(res.data);
+        const data = res.data;
         if (data) {
-           setWeather(data.weather[0].main);
+          setWeather(data.weather[0].main);
           setDescription(data.weather[0].description);
-          setTemp(data.main.temp)
-          // console.log(data.main)
+          setTemp(data.main.temp);
         }
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
       });
+  };
+  const toggleWeather = () => {
+    setShowWeather(true);
+    setShowForecast(false);
+  };
+
+  const toggleForecast = () => {
+    setShowWeather(false);
+    setShowForecast(true);
   };
 
   return (
@@ -51,16 +83,23 @@ export default function Search() {
           onChange={(e) => setLocation(e.target.value)}
         />
         <button onClick={handleSearch}>Search</button>
-        <CurrentWeather 
-        weather={weather} 
-        description={description} 
-        temp={temp} />
-        <Forecast />
+
+        {showWeather ? (
+          <CurrentWeather
+            weather={weather}
+            description={description}
+            temp={temp}
+          />
+        ) : (
+          <section>
+            <Forecast lat={latitude} lon={longitude} API={API_KEY} />
+          </section>
+        )}
 
         <div>
-      <button>Current</button>
-      <button>5 day</button>
-      </div>
+          <button onClick={toggleWeather}>Current</button>
+          <button onClick={toggleForecast}>5 day</button>
+        </div>
       </div>
     </section>
   );
